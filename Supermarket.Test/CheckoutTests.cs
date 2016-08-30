6dc670f0711b23364edd287a1f8dae07215eb11a
Cameron Hughes <cameron.hughes98@me.com>
2016-08-30 17:48:01 +0100

    checkout now takes in Item

--- Supermarket.Test/CheckoutTests.cs
+++ Supermarket.Test/CheckoutTests.cs
@@ -6,16 +6,34 @@ namespace Supermarket.Test
     [TestClass]
     public class CheckoutTests
     {
+        private const string _skuOfA = "A";
+        private const int _priceOfA = 50;
+
+        private const string _skuOfB = "B";
+        private const int _priceOfB = 30;
+
+        private Item _itemA = new Item(_skuOfA, _priceOfA);
+        private Item _itemB = new Item(_skuOfB, _priceOfB);
+
         [TestMethod]
         public void GivenIScanSkuOfAItShouldGiveTotalOf50()
         {
-            var checkout = new Checkout();
-            const string skuOfA = "A";
-            const int priceOfA = 50;
+            var checkout = new Checkout(_itemA, _itemB);
 
-            checkout.Scan(skuOfA);
+            checkout.Scan(_skuOfA);
 
-            Assert.AreEqual(priceOfA, checkout.Total);
+            Assert.AreEqual(_priceOfA, checkout.Total);
         }
+
+        [TestMethod]
+        public void GivenIScanSkuOfBItShouldGiveTotalOf30()
+        {
+            var checkout = new Checkout(_itemA, _itemB);
+
+            checkout.Scan(_skuOfB);
+
+            Assert.AreEqual(_priceOfB, checkout.Total);
+        }
+
     }
 }
--- Supermarket/Checkout.cs
+++ Supermarket/Checkout.cs
@@ -4,11 +4,35 @@ namespace Supermarket
 {
     public class Checkout
     {
-        public int Total { get; set; }
+        public int Total { get; private set; }
+
+        private Item _itemA;
+        private Item _itemB;
+
+        public Checkout(Item itemA, Item itemB)
+        {
+            _itemA = itemA;
+            _itemB = itemB; 
+        }
 
         public void Scan(string item)
         {
-            Total = 50;
+            if(item == _itemA.Sku)
+                Total = _itemA.Price;
+            if (item == _itemB.Sku)
+                Total = _itemB.Price;
+        }
+    }
+
+    public class Item
+    {
+        public string Sku { get; private set; }
+        public int Price { get; private set; }
+
+        public Item(string sku, int price)
+        {
+            Sku = sku;
+            Price = price;
         }
     }
 }
